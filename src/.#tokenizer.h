ce39a2089485874e7a3d2b0390b53ab95ae5ccbe
derekaguirre <derekaguirre@me.com>
2021-06-21 03:07:47 -0600

    Implemented the first four functions in tokenizer.c

---
+++ src/.#tokenizer.h
@@ -0,0 +1,1 @@
+student@systems-vm.2496:1623698116
---
+++ src/tokenizer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "tokenizer.h"
+
+int space_char(char c) {
+  if (c == ' ' || c == '\t' || c == '\n')
+    return 1;
+  return 0;
+}
+
+int non_space_char(char c) {
+  if(c != ' ' || c != '\t' || c != '\n')
+    return 1;
+  return 0;
+}
+
+/*Will iterate until a letter is found and returns a pointer to the beginning of a word*/
+char *word_start (char *str){
+  while(space_char(*str)){
+   str++;
+  }
+  return str;
+}
+
+/*Will iterate until a space character is found returns a pointer to a space*/
+char *word_terminator(char *word){
+  while(non_space_char(*word)){
+    word++;
+  }
+  return word;
+}
