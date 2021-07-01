307d04b525c8cb2e3a96d9b7719a34406c4173f4
derekaguirre <derekaguirre@me.com>
2021-06-30 18:52:42 -0600

    Implemented working ui with all functions implemented

--- src/uimain.c
+++ src/uimain.c
@@ -1,50 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
+
 
 int main() {
-  printf("Hello!\n");
-  char str[50];
+  
+  char user_input[100]; /*user input array*/
+  char **tokens; /*used for tokenize method*/
+  List *history = init_history();
+  
+  while(1){ /* run as long as the user does not specify quitting which breaks loop*/
+    printf("Enter desired string or command:\n\n");
+    printf("i  = Input a string into the program\n");
+    printf("h  = Print all entries in the history\n");
+    printf("!# = Print specified entry in the history\n");
+    printf("q  = Quit program\n\n");
+      
+    printf("$ ");
+    fgets(user_input,100,stdin);
 
-  printf("\nEnter a string:\n");
-  fgets(str,50,stdin);
-
-  printf("%s\n", str);
-  int letter_pos = 4;
-  printf("space_Char output\n");
-  printf("%d", space_char(str[letter_pos]));
-  printf("\n");
+    if(*user_input == 'i'){
+      printf("Input a string:\n");
+      fgets(user_input,100, stdin);
+      tokens = tokenize(user_input);
+      
+      printf("Tokenized string:\n");
+      print_tokens(tokens);
+      
+      printf("Adding input to history...\n");
+      add_history(history, user_input);
+      
+      printf("Freeing tokens from string...\n");
+      free_tokens(tokens);
+    }
     
-  printf("non_space_char output\n");
-  printf("%d", non_space_char(str[letter_pos]));
-  printf("\n");
+    else if(*user_input == 'h'){
+      printf("Printing history...\n");
+      print_history(history);
+    }
+    
+    else if (user_input[0] == '!'){
+      int index = user_input[1] - '0'; //converts char into int
 
-  printf("word_start output\n");
-  printf("%c",word_start(str));
-  printf("\n");
-
-  printf("word_terminator output\n");
-  printf("%c",word_terminator(str));
-  printf("\n");
-  
-  printf("copy_str output\n"); 
-  printf("%s", copy_str(str,sizeof(str)));
-  printf("\n");
-  
-  printf("count_words output\n");
-  printf("%d", count_words(str));
-  printf("\n");
-
-  char **tokens = tokenize(str);
-  
-  printf("print_tokens output\n");
-  print_tokens(tokens);
-  free_tokens(tokens);
-  printf("empty tokens\n");
-  print_tokens(tokens);
-  printf("end\n");
-  
-
-  
+      printf("%s",get_history(history,index)); //prints what history returns
+    }
+    
+    else if(*user_input == 'q'){
+      free_history(history);
+      printf("Exiting...\n\n");
+      break;
+    }
+    
+    else{
+      printf("\nCommand not found, please re-enter command\n\n");
+    }
+    
+  }
 }
-	     
